b2c4dbe54a5fb63813124032f859b01f3cad7cc6
Azerino Yogananta Gatot Subroto <azerino25@gmail.com>
2019-01-14 22:54:42 +0700

    Adding header and styling with styled-components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
+
+const Header = styled.header`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-family: "ZCOOL QingKe HuangYou", cursive;
+  font-size: 25pt;
+  padding: 20pt;
+  background: #ef6c57;
+  color: #ffffff;
+`;
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Header>
+        Pokémon Card Battle
+      </Header>
     );
   }
 }
